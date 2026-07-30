0d09d5ea34e70f86fe8e356f7d7e77a6b52e9c5b
FearCleevan <jonathan.mauring17@gmail.com>
2026-07-31 02:06:13 +0800

    Add Google Search Console verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,6 +105,10 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+
+  verification: {
+    google: "9LiilykrheaPPfueXX_EBnZHvijsZqBlNN-eM5uJfL4",
+  },
 };
 
 export default function RootLayout({
